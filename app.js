4cee5aa9557b326c475cd6d08ecc3582935c64d2
Ize Imai <izeimai@gmail.com>
2018-12-21 21:11:37 -0500

    All columns calculated and functional

--- app.js
+++ app.js
@@ -17,19 +17,18 @@ var database = firebase.database();
 $("#add-train").on("click", function (event) {
     event.preventDefault(); // prevents page from refreshing when hitting submit button
 
-    var trainName = $("name-input").val().trim();
-    var trainDest = $("destination-input)").val().trim();
-    var trainTime = $("time-input").val().trim();
-    var trainFreq = $("frequency-input").val().trim();
+    var trainName = $("#name-input").val().trim();
+    var trainDest = $("#destination-input").val().trim();
+    var trainTime = $("#time-input").val().trim();
+    var trainFreq = $("#frequency-input").val().trim();
 
 
-    // Push inputs to the dtabase
+    // Push inputs to the database
     database.ref().push({
         name: trainName,
         destination: trainDest,
         time: trainTime,
-        frequency: trainFreq,
-        dateAdded: firebase.database.ServerValue.TIMESTAMP
+        frequency: trainFreq
     });
 
 }); // close click event
@@ -38,16 +37,30 @@ $("#add-train").on("click", function (event) {
 database.ref().on("child_added", function (snapshot) {
     var snap = snapshot.val(); // store values
 
-    console.log(snap.name + snap.destination + snap.time + snap.frequency);
+    // Server time is available as moment()
+    var currentTime = moment();
+    // I could subtract a whole year, but 1 day seems sufficient for a daily train schedule
+    var firstTrainTime = moment(snap.time, "HH:mm").subtract(1, "days");
+    // Difference between current time and firstTrainTime in minutes
+    var timeDifference = moment().diff(moment(firstTrainTime), "minutes");
+    // Modulo used for the minutes remaining after difference is divided by frequency
+    var timeRemaining = timeDifference % snap.frequency;
+    // Subtract the remaining minutes from the frequency to get minutes until next train
+    var minutesToNextTrain = snap.frequency - timeRemaining;
+    // Add the minutes to next train to the current time and format to military time
+    var nextTrainTime = moment().add(minutesToNextTrain, "minutes").format("HH:mm");
 
     // Add rows to the HTML
     var newRow = $("<tr>").append(
         $("<td>").text(snap.name),
         $("<td>").text(snap.destination),
-        $("<td>").text(snap.freq),
-        $("<td>").text("Next Arrival"),
-        $("<td>").text("Minutes away")
+        $("<td>").text(snap.frequency),
+        $("<td>").text(nextTrainTime),
+        $("<td>").text(minutesToNextTrain)
     );
+
+    // Append the new row to the html
+    $("tbody").append(newRow);
 
 }, function (errorObject) { // Error
     console.log("Errors handled: " + errorObject.code);
